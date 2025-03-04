9680f1c5e6c2c41b8e2c2c1a3c146a279d26c620
aryannkaushikk <157898199+aryannkaushikk@users.noreply.github.com>
2025-03-04 21:57:07 +0530

    Create Array to DLL (GFG).java

---
+++ Doubly Linked List/Array to DLL (GFG).java
@@ -0,0 +1,13 @@
+class Solution {
+    Node constructDLL(int arr[]) {
+        Node head = new Node(arr[0]);
+        Node temp = head;
+        for(int i = 1; i<arr.length; i++){
+            Node t = new Node(arr[i]);
+            t.prev = temp;
+            temp.next = t;
+            temp = t;
+        }
+        return head;
+    }
+}
