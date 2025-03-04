6f7231177dacd2ac673683e49f727e7f9989d9ab
aryannkaushikk <157898199+aryannkaushikk@users.noreply.github.com>
2025-03-04 10:46:02 +0530

    Create Recover BST (L).java

---
+++ Binary Search Tree/Recover BST (L).java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private static TreeNode prev;
+    private static TreeNode first;
+    private static TreeNode last;
+    private static TreeNode middle;
+
+    public static void inorder(TreeNode root) {
+        if(root==null){
+            return;
+        }
+        inorder(root.left);
+        if(prev.val>root.val){
+            if(first==null){
+                first = prev;
+                middle = root;
+            }else{
+                last = root;
+            }
+        }
+        prev = root;
+        inorder(root.right);
+    }
+
+    public void recoverTree(TreeNode root) {
+        if(root==null){
+            return;
+        }
+        first = middle = last = null;
+        prev = new TreeNode(Integer.MIN_VALUE);
+        inorder(root);
+        if(last==null){
+            int temp = first.val;
+            first.val = middle.val;
+            middle.val = temp;
+        }else{
+            int temp = first.val;
+            first.val = last.val;
+            last.val = temp;
+        }
+    }
+}
