6d29c675bb1b1124645070177e27dcc337f40aa5
aryannkaushikk <157898199+aryannkaushikk@users.noreply.github.com>
2025-03-09 00:00:26 +0530

    Update Missing Number (L).java

--- Arrays/Missing Number (L).java
+++ Arrays/Missing Number (L).java
@@ -1,22 +1,33 @@
+// class Solution {
+//     public int missingNumber(int[] nums) {
+//         int[] n = new int[nums.length];
+//         int i = 0;
+//         while(i<nums.length){
+//             if(nums[i]>n.length-1){
+//                 i++;
+//                 continue;
+//             }
+//             n[nums[i]] = 1;
+//             i++;
+//         }
+//         i = 0;
+//         while(i<n.length){
+//             if(n[i]==0){
+//                 break;
+//             }
+//             i++;
+//         }
+//         return i;
+//     }
+// }
+
 class Solution {
     public int missingNumber(int[] nums) {
-        int[] n = new int[nums.length];
-        int i = 0;
-        while(i<nums.length){
-            if(nums[i]>n.length-1){
-                i++;
-                continue;
-            }
-            n[nums[i]] = 1;
-            i++;
+        int n = nums.length;
+        int x = 0;
+        for(int i = 0; i<n; i++){
+            x = x^nums[i]^i;
         }
-        i = 0;
-        while(i<n.length){
-            if(n[i]==0){
-                break;
-            }
-            i++;
-        }
-        return i;
+        return x^n;
     }
 }
