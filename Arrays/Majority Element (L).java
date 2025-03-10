770a432fe64fa119c326c678c81583cf0104c620
aryannkaushikk <157898199+aryannkaushikk@users.noreply.github.com>
2025-03-10 23:09:53 +0530

    Create Majority Element (L).java

---
+++ Arrays/Majority Element (L).java
@@ -0,0 +1,17 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int el = 0;
+        int count = 0;
+        for(int i = 0; i<nums.length; i++){
+            if(count==0){
+                el = nums[i];
+            }
+            if(nums[i]==el){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return el;
+    }
+}
