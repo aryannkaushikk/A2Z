f490ffe79d4a294f7432c013939b5e9cd0db862e
aryannkaushikk <157898199+aryannkaushikk@users.noreply.github.com>
2025-03-15 19:49:54 +0530

    Update Missing and Repeating Numbers (GFG).java

--- Arrays/Missing and Repeating Numbers (GFG).java
+++ Arrays/Missing and Repeating Numbers (GFG).java
@@ -1,4 +1,12 @@
 // Another approach invloving sum of numbers and sum of squares of numbers but need to use BigInteger class for that
+// In that approach you create two equations:
+// 1. (Sum of n numbers) - (Sum of all elements in array) -> x+y
+// 2. (Sum of sqaure of n numbers) - (Sum of sqaures of all elements in array) -> x^2 + y^2
+// Solve equations and you have x = missing number and y = repeating number
+
+// In this approach, I am simply storing 1 for all elements that appeared from 1 to n.
+// There will be a zero for one that did not appear and 2 for one that occured twice.
+
 class Solution {
     // Function to find two elements in array
     ArrayList<Integer> findTwoElement(int arr[]) {
