86d9c0a385b00d1932aafc0c6c7cb9a2c94bd4e4
aryannkaushikk <157898199+aryannkaushikk@users.noreply.github.com>
2025-03-15 19:26:10 +0530

    Update Array Leaders (GFG).java

--- Arrays/Array Leaders (GFG).java
+++ Arrays/Array Leaders (GFG).java
@@ -1,3 +1,7 @@
+// Since last element is always a leader you will add it to list and maintain a max which is initially last element.
+// Iterate from n-2 and when element>max add to list and set max = element.
+// In the end reverse the list to return in same order.
+
 class Solution {
     static ArrayList<Integer> leaders(int arr[]) {
         ArrayList<Integer> list = new ArrayList<>();
