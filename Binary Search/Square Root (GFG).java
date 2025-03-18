ce57bd5f5c0d41f1b1636071bd8d190c54e761d2
aryannkaushikk <157898199+aryannkaushikk@users.noreply.github.com>
2025-03-19 00:04:37 +0530

    Create Square Root (GFG).java

---
+++ Binary Search/Square Root (GFG).java
@@ -0,0 +1,22 @@
+// From range 0-n, you find mid and then if square of mid is less than you search in mid+1-n to find greatest number whose sqaure<=n, if square of mid>n go left i.e. 0 - mid-1, and keep storing value to check max value.
+
+class Solution {
+    
+    static int sqrt(int n, int[] ans, int low, int high){
+        if(low>high){
+            return ans[0];
+        }
+        int mid = (low+high)/2;
+        int sqr = mid*mid;
+        if(sqr<=n){
+            ans[0] = Math.max(ans[0],mid);
+            return sqrt(n,ans,mid+1,high);
+        }
+        return sqrt(n,ans,low,mid-1);
+    }
+    
+    int floorSqrt(int n) {
+        int[] ans = {Integer.MIN_VALUE};
+        return sqrt(n,ans,0,n);
+    }
+}
