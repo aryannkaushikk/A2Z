d8c88f89687f07146b4f17b3643738670d82e05d
aryannkaushikk <157898199+aryannkaushikk@users.noreply.github.com>
2025-03-30 20:54:42 +0530

    Update Maximum Path Sum (L).java

--- Binary Tree/Maximum Path Sum (L).java
+++ Binary Tree/Maximum Path Sum (L).java
@@ -1,3 +1,8 @@
+Calculate for every node its left sum and right sum
+Put max as l+r+root,l+root,r+root,root
+and return max of root or root+l+r
+
+Same as diameter we are calculating sum of node values for each node
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -15,31 +20,24 @@
  */
 class Solution {
 
-    private static int maxx;
-
-    private static int maxPathSumFinder(TreeNode root){
+    public static int pathSum(TreeNode root, int[] max){
         if(root==null){
             return 0;
         }
-        int l = maxPathSumFinder(root.left);
-        int r = maxPathSumFinder(root.right);
-        maxx = Math.max(maxx,l+r+root.val);
-        maxx = Math.max(maxx,r+root.val);
-        maxx = Math.max(maxx,l+root.val);
-        maxx = Math.max(maxx,root.val);
-        int all = root.val+Math.max(l,r);
-        if(root.val>all){
-            return root.val;
-        }
-        return all;
+        int l = pathSum(root.left,max);
+        int r = pathSum(root.right,max);
+
+        max[0] = Math.max(max[0],l+r+root.val);
+        max[0] = Math.max(max[0],l+root.val);
+        max[0] = Math.max(max[0],r+root.val);
+        max[0] = Math.max(max[0],root.val);
+
+        return Math.max(root.val,root.val+Math.max(l,r));
     }
 
     public int maxPathSum(TreeNode root) {
-        if(root==null){
-            return 0;
-        }
-        maxx = Integer.MIN_VALUE;
-        maxPathSumFinder(root);
-        return maxx;
+        int[] max = {Integer.MIN_VALUE};
+        pathSum(root,max);
+        return max[0];
     }
 }
