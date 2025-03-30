29bff49b1c1be8956cf09979c245731e86ac3c65
aryannkaushikk <157898199+aryannkaushikk@users.noreply.github.com>
2025-03-30 20:08:58 +0530

    Update BST Iterator (L).java

--- Binary Search Tree/BST Iterator (L).java
+++ Binary Search Tree/BST Iterator (L).java
@@ -1,44 +1,47 @@
+When the BSTIterator is instantiated pushAll all values of left from root
+When next is called pop from stack and pushAll for curr.right and return curr.val
+hasNext is answered by checking stack is empty or not
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class BSTIterator {
 
-    private static Stack<TreeNode> s;
-
+    static Stack<TreeNode> s;
     public BSTIterator(TreeNode root) {
         s = new Stack<>();
         pushAll(root);
     }
 
-    public static void pushAll(TreeNode root) {
-        if (root == null) {
+    public static void pushAll(TreeNode root){
+        if(root==null){
             return;
         }
         TreeNode temp = root;
-        while (temp != null) {
+        while(temp!=null){
             s.push(temp);
             temp = temp.left;
         }
     }
 
     public int next() {
-        TreeNode temp = s.pop();
-        pushAll(temp.right);
-        return temp.val;
+        TreeNode curr = s.pop();
+        pushAll(curr.right);
+        return curr.val;
     }
-
+    
     public boolean hasNext() {
         return !s.isEmpty();
     }
