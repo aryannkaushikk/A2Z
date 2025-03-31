f8a9ac6e0b5fe3a0f8eb8997890c52b2dceaa197
aryannkaushikk <157898199+aryannkaushikk@users.noreply.github.com>
2025-03-31 17:08:23 +0530

    Update Vertical Order Traversal (L).java

--- Binary Tree/Vertical Order Traversal (L).java
+++ Binary Tree/Vertical Order Traversal (L).java
@@ -15,74 +15,37 @@
  */
 class Solution {
 
-    private static class Tuple{
-        TreeNode n;
-        int x;
-        int l;
-
-        public Tuple(TreeNode n, int x, int l){
-            this.n = n;
-            this.l = l;
-            this.x = x;
+    public static void preOrder(TreeNode root, int x, int l, TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map){
+        if(root==null){
+            return;
         }
-    }
-
-    private static TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>> mapping(TreeNode root){
-        TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>> map = new TreeMap<>();
-        Queue<Tuple> q = new LinkedList<>();
-        q.offer(new Tuple(root,0,0));
-        while (!q.isEmpty()){
-            Tuple temp = q.poll();
-            TreeNode n = temp.n;
-            int x = temp.x;
-            int l = temp.l;
-            if(n.left!=null){
-                q.offer(new Tuple(n.left,x-1,l+1));
-            }
-            if(n.right!=null){
-                q.offer(new Tuple(n.right,x+1,l+1));
-            }
-            if(!map.containsKey(x)){
-                PriorityQueue<Integer> pq = new PriorityQueue<>();
-                pq.add(n.val);
-                TreeMap<Integer, PriorityQueue<Integer>> tmap = new TreeMap<>();
-                tmap.put(l,pq);
-                map.put(x,tmap);
-            }
-            else{
-                if(!map.get(x).containsKey(l)){
-                    PriorityQueue<Integer> pq = new PriorityQueue<>();
-                    pq.add(n.val);
-                    map.get(x).put(l,pq);
-                }
-                else{
-                    map.get(x).get(l).add(n.val);
-                }
-            }
+        if(!map.containsKey(x)){
+            map.put(x,new TreeMap<>());
         }
-        return map;
+        if(!map.get(x).containsKey(l)){
+            map.get(x).put(l,new PriorityQueue<>());
+        }
+        map.get(x).get(l).add(root.val);
+        preOrder(root.left,x-1,l+1,map);
+        preOrder(root.right,x+1,l+1,map);
     }
 
     public List<List<Integer>> verticalTraversal(TreeNode root) {
         if(root==null){
-            return new ArrayList<>();
+            return new LinkedList<>();
         }
-        TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>> map = mapping(root);
-        List<List<Integer>> list = new LinkedList<>();
-        for (Map.Entry<Integer,TreeMap<Integer,PriorityQueue<Integer>>> entry: map.entrySet()){
-            List<Integer> temp = new LinkedList<>();
-            for (Map.Entry<Integer,PriorityQueue<Integer>> entry2 : entry.getValue().entrySet()){
-                PriorityQueue<Integer> pqt = entry2.getValue();
-                if(pqt.size()==1){
-                    temp.add(pqt.poll());
-                    continue;
-                }
-                while (!pqt.isEmpty()){
-                    temp.add(pqt.poll());
+        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
+        List<List<Integer>> ans = new LinkedList<>();
+        preOrder(root,0,0,map);
+        for(Map.Entry<Integer, TreeMap<Integer, PriorityQueue<Integer>>> entry1: map.entrySet()){
+            List<Integer> list = new LinkedList<>();
+            for(Map.Entry<Integer, PriorityQueue<Integer>> entry2: entry1.getValue().entrySet()){
+                while(!entry2.getValue().isEmpty()){
+                    list.add(entry2.getValue().poll());
                 }
             }
-            list.add(temp);
+            ans.add(list);
         }
-        return list;
+        return ans;
     }
 }
