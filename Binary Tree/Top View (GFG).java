ce3734e0d5606e8f113360afc3b060c750b8e687
aryannkaushikk <157898199+aryannkaushikk@users.noreply.github.com>
2025-04-01 16:10:48 +0530

    Update Top View (GFG).java

--- Binary Tree/Top View (GFG).java
+++ Binary Tree/Top View (GFG).java
@@ -1,3 +1,5 @@
+Iterate in Level Order and keep storing in TreeMap in x:value pair if x does not exist
+
 /*
 class Node{
     int data;
@@ -14,53 +16,45 @@ class Node{
 class Solution {
     // Function to return a list of nodes visible from the top view
     // from left to right in Binary Tree.
-    private static class Tuple{
-        Node n;
+    
+    static class Pair{
+        Node root;
         int x;
-        int l;
-
-        public Tuple(Node n, int x, int l){
-            this.n = n;
-            this.l = l;
+        
+        public Pair(Node root, int x){
+            this.root = root;
             this.x = x;
         }
     }
-
-    private static TreeMap<Integer,TreeMap<Integer,Integer>> mapping(Node root){
-        TreeMap<Integer,TreeMap<Integer, Integer>> map = new TreeMap<>();
-        Queue<Tuple> q = new LinkedList<>();
-        q.offer(new Tuple(root,0,0));
-        while (!q.isEmpty()){
-            Tuple temp = q.poll();
-            Node n = temp.n;
-            int x = temp.x;
-            int l = temp.l;
+    
+    public static void levelOrder(Node root, TreeMap<Integer, Integer> map){
+        Queue<Pair> q = new LinkedList<>();
+        q.offer(new Pair(root,0));
+        while(!q.isEmpty()){
+            Pair p = q.poll();
+            Node n = p.root;
+            int x = p.x;
+            if(!map.containsKey(x)){
+                map.put(x,n.data);
+            }
             if(n.left!=null){
-                q.offer(new Tuple(n.left,x-1,l+1));
+                q.offer(new Pair(n.left,x-1));
             }
             if(n.right!=null){
-                q.offer(new Tuple(n.right,x+1,l+1));
-            }
-            if(!map.containsKey(x)){
-                TreeMap<Integer, Integer> tmap = new TreeMap<>();
-                tmap.put(l,n.data);
-                map.put(x,tmap);
+                q.offer(new Pair(n.right,x+1));
             }
         }
-        return map;
     }
-    
     
     static ArrayList<Integer> topView(Node root) {
         if(root==null){
             return new ArrayList<>();
         }
-        TreeMap<Integer,TreeMap<Integer, Integer>> map = mapping(root);
         ArrayList<Integer> list = new ArrayList<>();
-        for(Map.Entry<Integer, TreeMap<Integer, Integer>> entry: map.entrySet()){
-            for(Map.Entry<Integer, Integer> entry2 : entry.getValue().entrySet()){
-                list.add(entry2.getValue());
-            }
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        levelOrder(root,map);
+        for(Map.Entry<Integer, Integer> entry: map.entrySet()){
+            list.add(entry.getValue());
         }
         return list;
     }
