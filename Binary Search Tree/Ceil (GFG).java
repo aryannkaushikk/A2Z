0ce0a9229ccf1bf4c6b2c674c7e2dae1fa46bec9
aryannkaushikk <157898199+aryannkaushikk@users.noreply.github.com>
2025-04-01 20:22:27 +0530

    Update Ceil (GFG).java

--- Binary Search Tree/Ceil (GFG).java
+++ Binary Search Tree/Ceil (GFG).java
@@ -1,21 +1,27 @@
+If root>=key check for min with c then go left to find value smaller then root to check for minimum value >=key else go right
+
 class Tree {
-    static int c;
-    public static void ceil(Node root, int key){
-        if(root==null){
-            return;
-        }
+    // Function to return the ceil of given number in BST.
+    
+    void ceil(Node root, int key, int[] c) {
+        if (root == null) return;
         if(root.data>=key){
-            c = root.data;
-            ceil(root.left,key);
+            c[0] = Math.min(c[0],root.data);
         }
-        else{
-            ceil(root.right,key);
+        if(root.data<key){
+            ceil(root.right,key,c);
+        }else{
+            ceil(root.left,key,c);
         }
     }
+    
     int findCeil(Node root, int key) {
         if (root == null) return -1;
-        c = -1;
-        ceil(root,key);
-        return c;
+        int[] c = {Integer.MAX_VALUE};
+        ceil(root,key,c);
+        if(c[0]==Integer.MAX_VALUE){
+            return -1;
+        }
+        return c[0];
     }
 }
