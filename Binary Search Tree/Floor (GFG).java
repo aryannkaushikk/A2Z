0b15987e75f75867497071d265f917d39846c368
aryannkaushikk <157898199+aryannkaushikk@users.noreply.github.com>
2025-04-01 20:29:14 +0530

    Update Floor (GFG).java

--- Binary Search Tree/Floor (GFG).java
+++ Binary Search Tree/Floor (GFG).java
@@ -1,16 +1,18 @@
+Pretty much same to ceil reverse logic
+    
 class Solution {
-    
-    static int f;
-    
-    public static void fl(Node root, int x){
+   
+    static void floor(Node root, int x, int[] f){
         if(root==null){
             return;
         }
         if(root.data<=x){
-            f = root.data;
-            fl(root.right,x);
+            f[0] = Math.max(f[0],root.data);
+        }
+        if(root.data<x){
+            floor(root.right,x,f);
         }else{
-            fl(root.left,x);
+            floor(root.left,x,f);
         }
     }
     
@@ -18,8 +20,11 @@ class Solution {
         if(root==null){
             return -1;
         }
-        f = -1;
-        fl(root,x);
-        return f;
+        int[] f = {Integer.MIN_VALUE};
+        floor(root,x,f);
+        if(f[0]==Integer.MIN_VALUE){
+            return -1;
+        }
+        return f[0];
     }
 }
