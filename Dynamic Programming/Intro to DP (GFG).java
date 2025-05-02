dc4ef90ebf28d4417584238a2f5d844550efc8fa
aryannkaushikk <157898199+aryannkaushikk@users.noreply.github.com>
2025-05-02 12:59:03 +0530

    Create Intro to DP (GFG).java

---
+++ Dynamic Programming/Intro to DP (GFG).java
@@ -0,0 +1,31 @@
+class Solution {
+    
+    static final int MOD = 1000000007;
+
+    static long topDown(int n, long[] dp){
+        if(n==0 || n==1) return n;
+        if(dp[n]==-1){
+            dp[n] = ( topDown(n-1, dp)+topDown(n-2, dp) ) % MOD;
+        }
+        return dp[n];
+    }
+
+    static long topDown(int n) {
+        long[] dp = new long[n+1];
+        Arrays.fill(dp, -1);
+        return topDown(n,dp);
+    }
+
+    static long bottomUp(int n) {
+        if(n==0 || n==1) return n;
+        long pre1 = 0;
+        long pre2 = 1;
+        long val = 0;
+        for(int i = 2; i<=n; i++){
+            val = ( pre2+pre1 ) % MOD;
+            pre1 = pre2;
+            pre2 = val;
+        }
+        return val;
+    }
+}
