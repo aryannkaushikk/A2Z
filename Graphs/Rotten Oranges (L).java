15c84c5eed6e0f458bbaafe68faaf9c4c150b2ee
aryannkaushikk <157898199+aryannkaushikk@users.noreply.github.com>
2025-06-18 23:57:13 +0530

    Update Rotten Oranges (L).java

--- Graphs/Rotten Oranges (L).java
+++ Graphs/Rotten Oranges (L).java
@@ -6,68 +6,53 @@ Also begin the queue by pushing all rotten first
 """
 
 class Solution {
-
-    static int[] dx = {0,0,-1,1};
-    static int[] dy = {-1,1,0,0};
-
-    static class Pair{
-        int x;
-        int y;
-
-        public Pair(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-    int bfs(int[][] grid, List<Pair> list){
-        Queue<Pair> q = new LinkedList<>();
-        int count = 0;
-        for(Pair p: list){
-            q.offer(new Pair(p.x,p.y));
-        }
-
-        while(!q.isEmpty()){
-            boolean flag = false;
-            int size = q.size();
-            while(size!=0){
-                Pair p = q.poll();
-                int px = p.x;
-                int py = p.y;
-
-                for(int d = 0; d<4; d++){
-                    if(dx[d]+px<0 || dx[d]+px>=grid.length || dy[d]+py<0 || dy[d]+py>=grid[0].length || grid[dx[d]+px][dy[d]+py]==0 || grid[dx[d]+px][dy[d]+py]==2) continue;
-                    flag = true;
-                    grid[dx[d]+px][dy[d]+py] = 2;
-                    q.offer(new Pair(dx[d]+px, dy[d]+py));
+    public int orangesRotting(int[][] grid) {
+        Queue<int[]> rottenQueue = new LinkedList();
+        int fresh = 0;
+        for (int i = 0; i < grid.length; i++)
+        {
+            for (int j = 0; j < grid[0].length; j++)
+            {
+                if (grid[i][j] == 2)
+                {
+                    rottenQueue.add(new int[] {i, j});
                 }
+                if (grid[i][j] == 1)
+                {
+                    fresh++;
+                }
+            }
+        }
+
+        if (fresh == 0) return 0;
+
+        int timeCount = -1;
+        int[] rowDir = new int[] {0, -1, 0, 1};
+        int[] colDir = new int[] {-1, 0, 1, 0};
+        while (!rottenQueue.isEmpty())
+        {
+            int size = rottenQueue.size();
+            while (size > 0)
+            {
+                int[] currentOrange = rottenQueue.remove();
+                for (int dir = 0; dir < 4; dir++)
+                {
+                    int nextRow = currentOrange[0] + rowDir[dir];
+                    int nextCol = currentOrange[1] + colDir[dir];
+                    if (nextRow >= 0 && nextRow < grid.length && nextCol >= 0 && nextCol < grid[0].length && grid[nextRow][nextCol] == 1)
+                    {
+                        grid[nextRow][nextCol] = 2;
+                        rottenQueue.add(new int[] {nextRow, nextCol});
+                        fresh--;
+                    }
+                }
+
                 size--;
             }
-            if(flag) count++;
-        }
-        return count;
-    }
-
-    public int orangesRotting(int[][] grid) {
-        List<Pair> list = new ArrayList<>();
-        for(int i = 0; i<grid.length; i++){
-            for(int j = 0; j<grid[0].length; j++){
-                if(grid[i][j]==2){
-                    list.add(new Pair(i,j));
-                }
-            }
+            
+            timeCount++;
         }
 
-        int count = bfs(grid, list);
-
-        for(int i = 0; i<grid.length; i++){
-            for(int j = 0; j<grid[0].length; j++){
-                if(grid[i][j]==1){
-                    return -1;
-                }
-            }
-        }
-
-        return count;
+        return fresh == 0 ? timeCount : -1;
     }
 }
