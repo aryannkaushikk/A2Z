67e18c83bfa4cb2de5a924fda26d496d6af36a96
aryannkaushikk <157898199+aryannkaushikk@users.noreply.github.com>
2025-08-05 21:29:26 +0530

    Update Longest Substring Without Repeating Characters (L).java

--- Sliding Window & Two Pointers/Longest Substring Without Repeating Characters (L).java
+++ Sliding Window & Two Pointers/Longest Substring Without Repeating Characters (L).java
@@ -2,8 +2,7 @@
 Approach
 
 Simple 2 Pointer Approach
-Keep moving right pointer till you dont discover a duplicate character
-If you do move left to the last occurence of that character and remove every character on your way from map
+Keep moving right pointer till you dont discover a duplicate character and that character's last occurence falls between left and right (otherwise it doesnt matter)
 Then just put left to last occurence + 1
 """
 
@@ -14,16 +13,12 @@ class Solution {
         int right = 0;
         int len = 0;
         int n = s.length();
-        while(right<n){
+        while (right < n) {
             char ch = s.charAt(right);
-            if(map.containsKey(ch)){
-                int id = map.get(ch);
-                while( left != id ) {
-                    if(map.containsKey(s.charAt(left))) map.remove(s.charAt(left++));
-                }
-                left = id+1;
+            if (map.containsKey(ch) && left <= map.get(ch)) {
+                left = map.get(ch) + 1;
             }
-            len = Math.max(len, right-left+1);
+            len = Math.max(len, right - left + 1);
             map.put(ch, right);
             right++;
         }
