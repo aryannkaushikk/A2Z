28e88365544b107f5945254f882cee0dfb8c1254
aryannkaushikk <157898199+aryannkaushikk@users.noreply.github.com>
2025-08-13 13:04:33 +0530

    Update Palindrome LL (L).java

--- Singly Linked List/Palindrome LL (L).java
+++ Singly Linked List/Palindrome LL (L).java
@@ -1,48 +1,39 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+"""
+Approach
+
+Find the middle using slow-fast
+Reverse using while loop 
+iterate from head and prev(newHead/Tail of LL) till prev!=null
+to check for palindrome
+"""
+
 class Solution {
 
-    public static void reverse(ListNode t, ListNode[] p, ListNode n){
-        if(t==null){
-            return;
-        }
-        n = t.next;
-        t.next = p[0];
-        p[0] = t;
-        t = n;
-        reverse(t,p,n);
-    }
-
     public boolean isPalindrome(ListNode head) {
-        if(head==null || head.next==null){
-            return true;
-        }
+        if(head==null) return  true;
+        
         ListNode slow = head;
         ListNode fast = head;
-        while(fast.next!=null && fast.next.next!=null){
+
+        while(fast!=null && fast.next!=null){
             slow = slow.next;
             fast = fast.next.next;
         }
-        ListNode[] p = {null};
-        ListNode n = null;
-        reverse(slow.next,p,n);
-        ListNode temp = head;
-        ListNode temp2 = p[0];
-        while(temp!=null && temp2!=null){
-            if(temp.val!=temp2.val){
-                return false;
-            }
-            temp = temp.next;
-            temp2 = temp2.next;
+
+        ListNode prev = null;
+        while(slow!=null){
+            ListNode next = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = next; 
         }
+
+        while(prev!=null){
+            if(head.val!=prev.val) return false;
+            head = head.next;
+            prev = prev.next;
+        }
+
         return true;
     }
 }
