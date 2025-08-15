8fe730f03dfe448f75c975a5a71f7e2e776c3272
aryannkaushikk <157898199+aryannkaushikk@users.noreply.github.com>
2025-08-15 14:41:01 +0530

    Update Maximum Points You Can Obtain from Cards (L).java

--- Sliding Window & Two Pointers/Maximum Points You Can Obtain from Cards (L).java
+++ Sliding Window & Two Pointers/Maximum Points You Can Obtain from Cards (L).java
@@ -1,26 +1,28 @@
-// Take lsum and rsum
-// Initiate with sum of k values from left
-// Then compare lsum+rsum for max value and keep subtracting last element of lsum and adding to rsum from last of array
+"""
+Approach
 
+Two Pointer + Prefix Sum Approach
+
+Calc Prefix Sum from 0 -> k
+Then keep subtracting rightmost element in left sum and add leftmost of right sum
+Compare for max
+"""
 
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
+        int n = cardPoints.length;
         int lsum = 0;
-        int rsum = 0;
-        int l = 0;
-        int r = cardPoints.length-1;
-        for(l = 0; l<k; l++){
-            lsum += cardPoints[l];
-        }
-        int max = Integer.MIN_VALUE;
         for(int i = 0; i<k; i++){
-            if(lsum+rsum>max){
-                max = lsum+rsum;
-            }
-            lsum -= cardPoints[--l];
-            rsum += cardPoints[r--];
+            lsum += cardPoints[i];
         }
-        max = Math.max(max,lsum+rsum);
+        int rsum = 0;
+        int max = 0;
+        max = Math.max(max, lsum+rsum);
+        for(int i = 0; i<k; i++){
+            lsum -= cardPoints[k-i-1];
+            rsum += cardPoints[n-i-1];
+            if(max<lsum+rsum) max = lsum+rsum;
+        }
         return max;
     }
 }
