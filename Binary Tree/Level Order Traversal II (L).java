25199dd3e20920924693cf9fdb60d1fcecccfc73
aryannkaushikk <157898199+aryannkaushikk@users.noreply.github.com>
2025-09-07 18:22:36 +0530

    Update Level Order Traversal II (L).java

--- Binary Tree/Level Order Traversal II (L).java
+++ Binary Tree/Level Order Traversal II (L).java
@@ -1,3 +1,12 @@
+"""
+Approach
+
+One way is to simply collect levels from root to leaf then reverse
+Other way is to use recursion 
+Store each level call recursion using a new List of children of current Level
+Then after recurion push list to main List
+"""
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -14,35 +23,29 @@
  * }
  */
 class Solution {
+
+    void rec(List<TreeNode> list, List<List<Integer>> ans){
+        if(list.size()==0) return;
+        int size = list.size();
+        List<Integer> temp = new ArrayList<>();
+        List<TreeNode> nextList = new ArrayList<>();
+        int i = 0;
+        while(i < size){
+            TreeNode node = list.get(i++);
+            temp.add(node.val);
+            if(node.left!=null) nextList.add(node.left);
+            if(node.right!=null) nextList.add(node.right);
+        }
+        rec(nextList, ans);
+        ans.add(temp);
+    }
+
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        if(root==null){
-            return new LinkedList<>();
-        }
-        List<List<Integer>> list = new LinkedList<>();
-        Queue<TreeNode> q = new LinkedList<>();
-        List<Integer> temp = new LinkedList<>();
-        q.offer(root);
-        q.offer(null);
-        while(!q.isEmpty()){
-            TreeNode curr = q.poll();
-            if(curr==null){
-                list.add(temp);
-                if(q.isEmpty()){
-                    break;
-                }
-                temp = new LinkedList<>();
-                q.offer(null);
-                continue;
-            }
-            temp.add(curr.val);
-            if(curr.left!=null){
-                q.offer(curr.left);
-            }
-            if(curr.right!=null){
-                q.offer(curr.right);
-            }
-        }
-        Collections.reverse(list);
-        return list;
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root==null) return ans;
+        List<TreeNode> list = new ArrayList<>();
+        list.add(root);
+        rec(list, ans);
+        return ans;
     }
 }
